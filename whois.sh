03f13ac279725adf9d0d82f53ad575b3e962307d
Xavi Carrillo <xavi.carrillo@gmail.com>
2012-08-03 19:26:17 +0200

    Migrate /opt/scripts from SVN to GIT

--- whois.sh
+++ whois.sh
@@ -4,7 +4,7 @@
 
 SITES=`ls /var/www/vhosts/ | egrep -e ".com|.net|.org|.biz"`
 
-for site in `echo $SITES ; echo laseu.net` 
+for site in `echo $SITES` 
 do
 	/opt/scripts/domain-check-2.sh -d $site -x 30 -e xavi.carrillo@gmail.com
 done
