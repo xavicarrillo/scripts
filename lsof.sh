ab2b1449dfa70fa59106efda142f3799d8f229ee
Xavi Carrillo <xavi.carrillo@gmail.com>
2013-11-15 16:06:12 +0100

    Clean stuff up

--- lsof.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-LSOF=`lsof|wc -l`
-
-if [[ $LSOF -ge 10000 ]]
-then
-	echo lsof | mail -s "lsof: $LSOF" di4blo@gmail.com
-fi
--- qmail/reinjecta-tots-mails-servidor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-SQLPASSWORD=`grep PASS_MYSQL /opt/scripts/includes/Parametros.inc |awk -F\" {'print $4'}`
-
-for x in `mysql -uroot -p$SQLPASSWORD inlander -e "select username from alias"|grep -v \|`
-do
-	/opt/scripts/qmail/reinjecta-mail.sh $x
-done
